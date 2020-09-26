6b0b7e7cca699eca60ef342c2328b1eb2e776932
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-09-26 12:02:31 +0300

    Added bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
