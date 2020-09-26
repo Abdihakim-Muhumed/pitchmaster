c7e2ef25c536148a9b2733b872be43d183160d09
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-09-26 16:04:45 +0300

    Created base.html added navbar.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,18 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from flask_login import login_required
+from models import User, Pitch, Comment
 
 
 @main.route('/')
 def index():
     return render_template('Index.html')
 
-@main.route('/profile.html')
-def view_profile():
-    render_template('profile.html')
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
